1118bc41164b88e8518d9bc3159b4cbac9942235
amartya-mojo <amartya.singh@mojoboxx.com>
2024-01-27 23:09:49 +0530

    Setup the shared session between react and the extension

--- src/popup/App/App.tsx
+++ src/popup/App/App.tsx
@@ -1,6 +1,20 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState<any | []>([]);
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      // This will send a req to the frontend, which proxies over to the backend, and that way I can share the user between frontend and ext
+      let response = await axios.get(
+        "http://localhost:3000/auth/getCurrentUser"
+      );
+      console.log(response.data);
+      setUser(response.data);
+    };
+    getCurrentUser();
+  }, []);
   return <div className="App">Hello World</div>;
 }
 
